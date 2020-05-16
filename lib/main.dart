a743ad60a46fa953326e1de42c9c0c7fb479f4db
Rob Marquez <robienallen@yahoo.com>
2020-05-16 16:36:03 -0700

    More package and build debugs

--- lib/main.dart
+++ lib/main.dart
@@ -36,105 +36,107 @@ class _MyLoginPageState extends State<LoginPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: toastKey,
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            Container(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    // For App logo and name
-                    SizedBox(height: 40.0),
-                    Column(
-                      children: <Widget>[
-                        Image.asset("assets/app_logo.jpg"),
-                        SizedBox(height: 20.0),
-                        Text(
-                          "Find-a-Flick",
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            fontStyle: FontStyle.italic
+    return SafeArea(
+      child: Scaffold(
+        key: toastKey,
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              Container(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      // For App logo and name
+                      SizedBox(height: 40.0),
+                      Column(
+                        children: <Widget>[
+                          Image.asset("assets/app_logo.jpg"),
+                          SizedBox(height: 20.0),
+                          Text(
+                            "Find-a-Flick",
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontStyle: FontStyle.italic
+                            ),
                           ),
+                        ],
+                      ),
+
+                      // For Email
+                      SizedBox(height: 30.0),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          filled: true,
                         ),
-                      ],
-                    ),
-
-                    // For Email
-                    SizedBox(height: 30.0),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        filled: true,
+                        validator: (String email) {
+                          if (email.trim().isEmpty) {
+                            return 'Email is required';
+                          }
+                        },
+                        onSaved: (input) => _email = input,
                       ),
-                      validator: (String email) {
-                        if (email.trim().isEmpty) {
-                          return 'Email is required';
-                        }
-                      },
-                      onSaved: (input) => _email = input,
-                    ),
 
-                    // For password
-                    SizedBox(height: 10.0),
-                    TextFormField(
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                        filled: true,
+                      // For password
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          filled: true,
+                        ),
+                        validator: (String password) {
+                          if (password.trim().isEmpty) {
+                            return 'Password is required';
+                          }
+                        },
+                        onSaved: (input) => _password = input,
                       ),
-                      validator: (String password) {
-                        if (password.trim().isEmpty) {
-                          return 'Password is required';
-                        }
-                      },
-                      onSaved: (input) => _password = input,
-                    ),
 
-                    // For Login Button
-                    SizedBox(height: 20.0),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          RaisedButton(
-                            onPressed: emailLogin,
-                            child: Text('Login w/ Email'), color: Colors.orange,
-                          ),
-                          RaisedButton(
-                            onPressed: face_login,
-                            child: Text('Login w/ Face'), color: Colors.orange,
-                          ),
-                        ],
+                      // For Login Button
+                      SizedBox(height: 20.0),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            RaisedButton(
+                              onPressed: emailLogin,
+                              child: Text('Login w/ Email'), color: Colors.orange,
+                            ),
+                            RaisedButton(
+                              onPressed: face_login,
+                              child: Text('Login w/ Face'), color: Colors.orange,
+                            ),
+                          ],
+                        )
+                      ),
+
+                      SizedBox(height: 2.0),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            RaisedButton(
+                              onPressed: emailRegister,
+                              child: Text('Register w/ Email'), color: Colors.orange,
+                            ),
+                            RaisedButton(
+                              onPressed: face_register,
+                              child: Text('Register w/ Face'), color: Colors.orange,
+                            ),
+                          ],
+                        ),
                       )
-                    ),
-
-                    SizedBox(height: 2.0),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          RaisedButton(
-                            onPressed: emailRegister,
-                            child: Text('Register w/ Email'), color: Colors.orange,
-                          ),
-                          RaisedButton(
-                            onPressed: face_register,
-                            child: Text('Register w/ Face'), color: Colors.orange,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
